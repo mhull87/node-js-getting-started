c61c80ddd0e57f5ca3fbce18e76964677effd749
mhull87 <64169244+mhull87@users.noreply.github.com>
2021-03-16 10:35:32 -0500

    Update moviecs.js

--- public/moviecs.js
+++ public/moviecs.js
@@ -16,12 +16,20 @@ const fetchData = async () => {
 function display(movies) {
   for (var i = 0; i < movies.Search.length; i ++) {
     var output = document.getElementById('output');
+output.innerHtml = "";
     var movie = movies.Search[i];
     var li = document.createElement("li");
     var text = document.createTextNode(movie.Title);
+var button = document.createElement("button");
+button.addEventListener('click', details(movie));
     li.appendChild(text);
+li.appendChild(button);
     output.appendChild(li);
 
   } 
 }
-
+function details(movie) {
+var output = document.getElementById("output").innerHtml = "";
+var title = document.createElement('li');
+title.createTextNode(movie.Title);
+}
