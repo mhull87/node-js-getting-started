a00ff3386b018cad7ab6e8cd324323fdb1a2361c
Melissa Hull <melissahull87@gmail.com>
2021-03-16 13:24:38 -0500

    team activity

--- public/moviecs.js
+++ public/moviecs.js
@@ -3,33 +3,37 @@ const apiURL = 'https://www.omdbapi.com/?apikey=5a713c14&s=';
 const fetchData = async () => {
   try {
     var search = document.getElementById('search').value;
-
+    console.log(search);
     const response = await fetch(`${apiURL}${search}`)
     const movies = await response.json()
     display(movies)
-  }
-  catch (err) {
+  } catch (err) {
     console.log(err)
   }
 }
 
 function display(movies) {
-  for (var i = 0; i < movies.Search.length; i ++) {
-    var output = document.getElementById('output');
-output.innerHtml = "";
+  document.getElementById("output").innerHTML = "";
+  document.getElementById("imageID").src = "";      
+  for (var i = 0; i < movies.Search.length; i++) {
     var movie = movies.Search[i];
     var li = document.createElement("li");
-    var text = document.createTextNode(movie.Title);
-var button = document.createElement("button");
-button.addEventListener('click', details(movie));
-    li.appendChild(text);
-li.appendChild(button);
+    li.innerHTML = `${movie.Title}<button onclick='details(${JSON.stringify(movie)})'>Details</button>`;
     output.appendChild(li);
-
-  } 
+  }
 }
-function details(movie) {
-var output = document.getElementById("output").innerHtml = "";
-var title = document.createElement('li');
-title.createTextNode(movie.Title);
+
+function details(myData) {
+  document.getElementById("output").innerHTML = "";
+  var movie = document.getElementById("output");
+  var p1 = document.createElement("p");
+  var p2 = document.createElement("p");
+  var p3 = document.createElement("p");
+  p1.innerHTML = myData.Title;
+  p2.innerHTML = myData.Year;
+  p3.innerHTML = "ID: " + myData.imdbID;
+  movie.appendChild(p1);
+  movie.appendChild(p2);
+  movie.appendChild(p3);
+  document.getElementById("imageID").src = myData.Poster;
 }
